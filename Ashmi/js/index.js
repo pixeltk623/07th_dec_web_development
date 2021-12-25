3afba1cd08732c7cff73a154341a7ba0fcf807ed
ashmi <ashmimaheshwari08@gmail.com>
2021-12-25 11:12:52 +0530

    done

---
+++ Ashmi/js/index.js
@@ -0,0 +1,31 @@
+
+//diffrence between two numbers
+
+const diff = (a, b) => {
+    return Math.abs(a - b);
+}
+
+console.log(diff(12, 17));
+
+//comaprison operator
+
+let a=20;
+ 
+ b=30;
+
+ console.log(a==b);
+
+ console.log(a>=b)
+
+ console.log(a<=b)
+
+ //increment operator
+
+console.log(a)
+
+console.log(a++)
+
+console.log(++a)
+
+console.log(a)
+
