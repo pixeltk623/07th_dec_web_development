703866d386b83071aead6cef8858164693af8558
ashmi <ashmimaheshwari08@gmail.com>
2022-01-06 18:37:09 +0530

    done

---
+++ Ashmi/countof digits.js
@@ -0,0 +1,9 @@
+
+function countdigits(){
+	const 123
+     let sum;
+	for( let i=0;i<=10;i++){
+    
+	}
+}
+console.log(sum=const);
--- Ashmi/js/index.js
+++ Ashmi/js/index.js
@@ -1,154 +1,154 @@
 
 //diffrence between two numbers
 
-const diff = (a, b) => {
-    return Math.abs(a - b);
-}
+// const diff = (a, b) => {
+//     return Math.abs(a - b);
+// }
 
-console.log(diff(12, 17));
+// console.log(diff(12, 17));
 
 //comaprison operator
 
-let a=20;
+// let a=20;
  
- b=30;
+//  b=30;
 
- console.log(a==b);
+//  console.log(a==b);
 
- console.log(a>=b)
+//  console.log(a>=b)
 
- console.log(a<=b)
+//  console.log(a<=b)
 
- //increment operator
+//  //increment operator
 
-console.log(a)
+// console.log(a)
 
-console.log(a++)
+// console.log(a++)
 
-console.log(++a)
+// console.log(++a)
 
-console.log(a)
+// console.log(a)
 
 
 //area of rectangle
-  let l, w, c;
-    l = 8;
-    w = 6;
-  console.log  (c = l * w);
+//   let l, w, c;
+//     l = 8;
+//     w = 6;
+//   console.log  (c = l * w);
     
 
-    //area of circle
-function circleArea(radius) {
-    let area = Math.PI * (radius * radius);
-    console.log(area);
-    console.log(Math.round(area*100)/100);
-}
+//     //area of circle
+// function circleArea(radius) {
+//     let area = Math.PI * (radius * radius);
+//     console.log(area);
+//     console.log(Math.round(area*100)/100);
+// }
  
-circleArea(5);
+// circleArea(5);
 
 
 
 
 //q1.prime no
-function test_prime(n)
-{
+// function test_prime(n)
+// {
 
-  if (n===1)
-  {
-    return false;
-  }
-  else if(n === 2)
-  {
-    return true;
-  }else
-  {
-    for(let x = 2; x < n; x++)
-    {
-      if(n % x === 0)
-      {
-        return false;
-      }
-    }
-    return true;  
-  }
-}
+//   if (n===1)
+//   {
+//     return false;
+//   }
+//   else if(n === 2)
+//   {
+//     return true;
+//   }else
+//   {
+//     for(let x = 2; x < n; x++)
+//     {
+//       if(n % x === 0)
+//       {
+//         return false;
+//       }
+//     }
+//     return true;  
+//   }
+// }
 
-console.log(test_prime(37));
+// console.log(test_prime(37));
 
 
 
 //  q2.prime no 1 to 100
 
-let count=0
-let i,j 
-for(j=2;j<=100;j++)
-{
-for( i=1;i<=j;i++)
-{
-  if(j%i==0)
-  count++
+// let count=0
+// let i,j 
+// for(j=2;j<=100;j++)
+// {
+// for( i=1;i<=j;i++)
+// {
+//   if(j%i==0)
+//   count++
   
-}
+// }
 
-if(count==2)
+// if(count==2)
 
-console.log(j)
-count=0
+// console.log(j)
+// count=0
 
 
   
-}
+// }
 
 //q3.reverse number 
 
-const num = 124323;
-const reverse = (num) => parseInt(String(num)
-.split("")
-.reverse()
-.join(""), 10);
-console.log(reverse(num));
+// const num = 124323;
+// const reverse = (num) => parseInt(String(num)
+// .split("")
+// .reverse()
+// .join(""), 10);
+// console.log(reverse(num));
 
 
 
 //q4.armstrong number
-function isArmstrong (n) {
-    const res = parseInt(n, 10) === String(n)
-        .split('')
-        .reduce((sum, n) => parseInt(sum, 10) + n ** 3, 0);
-    console.log(n, 'is', res, 'Armstrong number')
-    return res
-}
+// function isArmstrong (n) {
+//     const res = parseInt(n, 10) === String(n)
+//         .split('')
+//         .reduce((sum, n) => parseInt(sum, 10) + n ** 3, 0);
+//     console.log(n, 'is', res, 'Armstrong number')
+//     return res
+// }
 
-isArmstrong(153)
+// isArmstrong(153)
 
 
 
 //q5.count of digits
-const number = 4567 ;
-const digits = (number, count = 0) => {
-   if(number){
-      return digits(Math.floor(number / 10), ++count);
-   };
-   return count;
-};
-console.log(digits(number));
+// const number = 4567 ;
+// const digits = (number, count = 0) => {
+//    if(number){
+//       return digits(Math.floor(number / 10), ++count);
+//    };
+//    return count;
+// };
+// console.log(digits(number));
 
 
 //q6.palindrome number
-function isPalindrome(n) {
-    if (n == reverse(n)) return true
-}
+// function isPalindrome(n) {
+//     if (n == reverse(n)) return true
+// }
 
-function sumArray(arr) {
-    var sum = 0
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] > 10 && isPalindrome(arr[i])) {
-            sum += arr[i]
-        }
+// function sumArray(arr) {
+//     var sum = 0
+//     for (var i = 0; i < arr.length; i++) {
+//         if (arr[i] > 10 && isPalindrome(arr[i])) {
+//             sum += arr[i]
+//         }
         
-    }
-    console.log(sum);    
-}
+//     }
+//     console.log(sum);    
+// }
 
 
-sumArray([313])
+// sumArray([313])
